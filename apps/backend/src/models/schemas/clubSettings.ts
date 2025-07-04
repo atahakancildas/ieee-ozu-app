b0b4fe6aef0488e5076a92e9f1f2488b2d0e8d02
atahakancildas <cildasata@gmail.com>
2025-07-04 21:05:33 +0300

    fix: change timestamp defaults in club settings

--- apps/backend/src/models/schemas/clubSettings.ts
+++ apps/backend/src/models/schemas/clubSettings.ts
@@ -8,21 +8,13 @@ export const clubSettings = pgTable("club_settings", {
   areSocietyApplicationsOpen: boolean("are_society_applications_open")
     .notNull()
     .default(false),
-  societyApplicationsOpenDate: timestamp(
-    "society_applications_open_date"
-  ).notNull(),
-  societyApplicationsCloseDate: timestamp(
-    "society_applications_close_date"
-  ).notNull(),
+  societyApplicationsOpenDate: timestamp("society_applications_open_date"),
+  societyApplicationsCloseDate: timestamp("society_applications_close_date"),
   areSocietyInterviewsOpen: boolean("are_society_interviews_open")
     .notNull()
     .default(false),
-  societyInterviewsOpenDate: timestamp(
-    "society_interviews_open_date"
-  ).notNull(),
-  societyInterviewsCloseDate: timestamp(
-    "society_interviews_close_date"
-  ).notNull(),
+  societyInterviewsOpenDate: timestamp("society_interviews_open_date"),
+  societyInterviewsCloseDate: timestamp("society_interviews_close_date"),
   areOpenPositionApplicationsOpen: boolean(
     "are_open_position_applications_open"
   )
@@ -30,10 +22,10 @@ export const clubSettings = pgTable("club_settings", {
     .default(false),
   openPositionApplicationsOpenDate: timestamp(
     "open_position_applications_open_date"
-  ).notNull(),
+  ),
   openPositionApplicationsCloseDate: timestamp(
     "open_position_applications_close_date"
-  ).notNull(),
+  ),
   updatedAt: timestamp("updated_at")
     .defaultNow()
     .$onUpdateFn(() => new Date()),
